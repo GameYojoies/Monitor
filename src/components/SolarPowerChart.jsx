08e2c27265c6cf6753d442c6fbb55ec22a32233e
Aoffy <116052697+AoffyYojoies@users.noreply.github.com>
2024-06-27 11:00:17 +0700

    update

--- src/components/SolarPowerChart.jsx
+++ src/components/SolarPowerChart.jsx
@@ -9,7 +9,7 @@ const SolarPowerChart = () => {
 
   return (
     <div className="w-full flex justify-center mt-10 mb-10">
-      <div className="w-[95%] flex flex-col lg:flex-row justify-center md:justify-between gap-10">
+      <div className="w-[95%] flex flex-col lg:flex-row justify-center lg:justify-between gap-10">
         <div className="w-[100%] lg:w-[70%] flex flex-col">
           <SolarPowerChartCom/>
         </div>
