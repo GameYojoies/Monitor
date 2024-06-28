3007097549a67e554100fae4d0c0a57f8860314d
Aoffy <116052697+AoffyYojoies@users.noreply.github.com>
2024-06-28 11:23:04 +0700

    update lang log out

--- src/components/modalConfirm.jsx
+++ src/components/modalConfirm.jsx
@@ -1,6 +1,9 @@
 /** @format */
 
-const ModalConfirm = ({onClose, onSave, text, header, titleButtom, icon}) => {
+
+
+const ModalConfirm = ({onClose, onSave, text, header, titleButtom, icon, textCancel}) => {
+
   return (
     <>
       <div
@@ -32,7 +35,7 @@ const ModalConfirm = ({onClose, onSave, text, header, titleButtom, icon}) => {
                       onClose()
                     }}
                     className="w-[143px] h-[48px] inline-flex justify-center items-center py-2 px-8 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400">
-                    Cancel
+                    {textCancel}
                   </button>
                   <button
                     onClick={() => {
